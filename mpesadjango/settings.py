52f92f164d1d6a8f7d17dfd8ef1603a17717270d
Mwalugha <mwalughabura@gmail.com>
2022-07-26 12:50:27 +0300

    Created the mpesa API intergration app

--- mpesadjango/settings.py
+++ mpesadjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mpesa_api.apps.MpesaApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
